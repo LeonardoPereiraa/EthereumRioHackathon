633ceedf2fc7b45e20c6cd52a518620a2c478fcb
gabrielsdev <89107888+gabrielsdev@users.noreply.github.com>
2022-03-19 21:50:22 -0300

    new changes

--- Impacto.sol
+++ Impacto.sol
@@ -7,7 +7,7 @@ contract Impacto {
         uint128 stepId;
         string text;
         uint256 cost;
-        address donor; // if address(0) no completed
+        address donor; // é diferente de address(0) se alguém doar apenas para a etapa
     }
 
     struct Projeto {
@@ -16,6 +16,7 @@ contract Impacto {
         address payable ongAddress;
         uint128 quantEtapas;
         uint256 totalCost;
+        address donor; // é diferente de address(0) se alguém doar para o projeto todo
     }
 
     mapping(uint128 => Projeto) private projetos;
@@ -23,10 +24,20 @@ contract Impacto {
     mapping(uint128 => uint128) quantEtapasConcluidas; // armazena a quantidade de etapas concluídas de cada projeto
     uint128[] private indexOf; // guarda os identificadores dos projetos
 
-    constructor () payable {}
+    address[] private doadores;
+    mapping (address => mapping (uint128 => uint128)) valorDoado; // quanto um doador x doou para um projecto y
+
+    constructor() payable {}
+
+    receive() external payable {}
     
     modifier onlyOng(uint128 id){
         require(projetos[id].ongAddress == msg.sender, "Not permitted");
+        _;
+    }
+
+    modifier availableToPay(uint128 id){
+        require(!projetos[id].concluded, "Not available");
         _;
     }
 
@@ -40,7 +51,6 @@ contract Impacto {
     ) external {
         require(msg.sender == _ongAddress, "Not permitted");
         uint128 id = uint128(indexOf.length);
-        projetos[id].concluded = false;
         projetos[id].projectName = _projectName;
         projetos[id].ongAddress = _ongAddress;
         projetos[id].quantEtapas = _quantTotalEtapas;
@@ -64,19 +74,42 @@ contract Impacto {
         projetos[id].totalCost = _totalCost;
     }
 
-    //function changeVisibility(uint128 id, uint128 _status) public onlyOng(id) { // only ong modifier
-        //projetos[id].projectStatus = _status;
-    //}
-
     function isConcluded(uint128 id) public view returns (bool) {
         return projetos[id].concluded;
     }
 
-    //TODO:
-    //function etapaConcluida(uint128 id, uint128 stepId) public onlyOng(id) {
-    function etapaConcluida(uint128 id) public onlyOng(id) {
-        if(projetos[id].quantEtapas == quantEtapasConcluidas[id]){
-            projetos[id].concluded = true;
+
+    //metodos get
+
+    //function getProjects() returns (uint128[]){
+        //return indexOf;
+    //}
+
+    // essa eh uma funcao segura? qual pode ser a vulnerabildade? 
+    function donation(uint128 projectId, uint128 stepId, bool payment) external availableToPay(projectId) payable { // false - paga o projeto inteiro; true - paga uma etapa
+        if (payment){
+            require(msg.value >= etapas[projectId][stepId].cost, "Valor menor que o esperado");
+            address payable x = projetos[projectId].ongAddress;
+            sendViaCall(x);
+            etapas[projectId][stepId].donor = msg.sender;
+            quantEtapasConcluidas[projectId]++;
+        } else {
+            require(msg.value >= projetos[projectId].totalCost, "Valor menor que o esperado");
+            address payable x = projetos[projectId].ongAddress;
+            sendViaCall(x);
+            projetos[projectId].donor = msg.sender;
+            quantEtapasConcluidas[projectId] = projetos[projectId].quantEtapas;
         }
+        
+        if(projetos[projectId].quantEtapas == quantEtapasConcluidas[projectId]){
+            projetos[projectId].concluded = true;
+        }
+    }
+
+    function sendViaCall(address payable _to) public payable {
+        // Call returns a boolean value indicating success or failure.
+        // This is the current recommended method to use.
+        (bool sent,) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
     }
 }
